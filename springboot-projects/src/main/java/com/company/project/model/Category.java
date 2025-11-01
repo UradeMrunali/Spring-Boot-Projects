46cb090d343a67f750fd86acac4ffe69127920dc
Mrunali Urade <124710812+UradeMrunali@users.noreply.github.com>
2025-11-01 13:36:44 +0530

    Spring Boot Project

--- springboot-projects/src/main/java/com/company/project/model/Category.java
+++ springboot-projects/src/main/java/com/company/project/model/Category.java
@@ -9,10 +9,10 @@ import java.util.UUID;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Category {
-
     private UUID categoryId;
     private String name;
     private UUID userId;
     private LocalDateTime createdAt;
 }
+
 
