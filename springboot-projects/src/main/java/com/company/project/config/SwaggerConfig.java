e94e44e6e6c2e0a064b731f9b1320b68c2d8935c
Mrunali Urade <124710812+UradeMrunali@users.noreply.github.com>
2025-11-16 13:45:38 +0530

    Spring Boot Project

--- springboot-projects/src/main/java/com/company/project/config/SwaggerConfig.java
+++ springboot-projects/src/main/java/com/company/project/config/SwaggerConfig.java
@@ -9,9 +9,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
-@Bean
-
-    
+@Bean    
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
@@ -20,4 +18,5 @@ public class SwaggerConfig {
                 .build();
     }
 }
+
 
